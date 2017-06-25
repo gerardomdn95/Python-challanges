09b2da7da20635cb47490bf2d30e8b8ebc55580a
Gerardo Medina <gerardo.mdn95@gmail.com>
2017-06-25 13:08:03 -0500

    level1_ex2.py Done

--- Level_1/level1_ex2.py
+++ Level_1/level1_ex2.py
@@ -1,6 +1,6 @@
 """
-Write a program which accepts a sequence of comma-separated numbers from console and generate a list and a tuple which contains every number.
-Suppose the following input is supplied to the program:
+Write a program which accepts a sequence of comma-separated numbers from console and generate a list
+and a tuple which contains every number. Suppose the following input is supplied to the program:
 34,67,55,33,12,98
 Then, the output should be:
 ['34', '67', '55', '33', '12', '98']
@@ -10,3 +10,9 @@ Hints:
 In case of input data being supplied to the question, it should be assumed to be a console input.
 tuple() method can convert list to tuple
 """
+
+values = input("Input a sequence o of comma-separated numbers: ")
+list = values.split(",")
+tuple = tuple(list)
+print(list)
+print(tuple)
