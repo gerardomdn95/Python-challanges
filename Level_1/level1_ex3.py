e8fdacd32df2c488aba6e50dce66c65030cc3183
Gerardo Medina <gerardo.mdn95@gmail.com>
2017-06-26 15:50:38 -0500

    level1_ex3.py Done

--- Level_1/level1_ex3.py
+++ Level_1/level1_ex3.py
@@ -5,6 +5,20 @@ printString: to print the string in upper case.
 
 Hint
 Use __init__ method to construct some parameters
-
-
 """
+
+class ex3:
+
+    def __init__(self):
+        self.mess = "Mensaje"
+
+    def getString(self):
+        self.mess = input("Type something: ")
+        return print(self.mess)
+
+    def printString(self):
+        print(self.mess.upper())
+
+level1_ex3 = ex3()
+level1_ex3.getString()
+level1_ex3.printString()
